d8b96d7351c0960b6dec52a50069723a2add238b
Scott <Scotthall1983@hotmail.com>
2019-10-11 10:36:58 +0100

    Added a basic react test structure to ensure content is rendering to the DOM

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// import the React & ReactDOM libraries
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (<div>Test</div>);
+};
+
+// Take React component and show it on the screen
+
+ReactDOM.render(<App />, document.querySelector('#root'));
